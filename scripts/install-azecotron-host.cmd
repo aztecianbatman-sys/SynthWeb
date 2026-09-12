735fa4630e22784ccddf5200dc330580f92da1c4
Fulltrack <aztecianbatman@gmail.com>
2026-09-12 21:12:32 +0530

    build: install complete Synth-owned Chromium app and Shield sources

--- scripts/install-azecotron-host.cmd
+++ scripts/install-azecotron-host.cmd
@@ -20,6 +20,16 @@ copy /Y "%ROOT%\azecotron\host\azecotron_runtime_host.cc" "%HOST_DST%\azecotron_
 copy /Y "%ROOT%\azecotron\host\BUILD.gn" "%HOST_DST%\BUILD.gn" >nul
 copy /Y "%ROOT%\azecotron\app\azecotron_app_main.cc" "%APP_DST%\azecotron_app_main.cc" >nul
 copy /Y "%ROOT%\azecotron\app\BUILD.gn" "%APP_DST%\BUILD.gn" >nul
+copy /Y "%ROOT%\azecotron\app\synth_browser_context.h" "%APP_DST%\synth_browser_context.h" >nul
+copy /Y "%ROOT%\azecotron\app\synth_browser_context.cc" "%APP_DST%\synth_browser_context.cc" >nul
+copy /Y "%ROOT%\azecotron\app\synth_browser_main_parts.h" "%APP_DST%\synth_browser_main_parts.h" >nul
+copy /Y "%ROOT%\azecotron\app\synth_browser_main_parts.cc" "%APP_DST%\synth_browser_main_parts.cc" >nul
+copy /Y "%ROOT%\azecotron\app\synth_content_browser_client.h" "%APP_DST%\synth_content_browser_client.h" >nul
+copy /Y "%ROOT%\azecotron\app\synth_content_browser_client.cc" "%APP_DST%\synth_content_browser_client.cc" >nul
+copy /Y "%ROOT%\azecotron\app\synth_content_main_delegate.h" "%APP_DST%\synth_content_main_delegate.h" >nul
+copy /Y "%ROOT%\azecotron\app\synth_content_main_delegate.cc" "%APP_DST%\synth_content_main_delegate.cc" >nul
+copy /Y "%ROOT%\azecotron\app\synth_tracker_throttle.h" "%APP_DST%\synth_tracker_throttle.h" >nul
+copy /Y "%ROOT%\azecotron\app\synth_tracker_throttle.cc" "%APP_DST%\synth_tracker_throttle.cc" >nul
 
 echo Native Azecotron Content API host installed at:
 echo %HOST_DST%
