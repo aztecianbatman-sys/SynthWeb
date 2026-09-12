6e1aa9fedb81114e88161c47fa9c3c01b61e5d5d
Fulltrack <aztecianbatman@gmail.com>
2026-09-12 20:40:27 +0530

    fix: install native Azecotron app sources before installer exit

--- scripts/install-azecotron-host.cmd
+++ scripts/install-azecotron-host.cmd
@@ -18,10 +18,10 @@ if not exist "%APP_DST%" mkdir "%APP_DST%"
 copy /Y "%ROOT%\azecotron\host\azecotron_runtime_host.h" "%HOST_DST%\azecotron_runtime_host.h" >nul
 copy /Y "%ROOT%\azecotron\host\azecotron_runtime_host.cc" "%HOST_DST%\azecotron_runtime_host.cc" >nul
 copy /Y "%ROOT%\azecotron\host\BUILD.gn" "%HOST_DST%\BUILD.gn" >nul
+copy /Y "%ROOT%\azecotron\app\azecotron_app_main.cc" "%APP_DST%\azecotron_app_main.cc" >nul
+copy /Y "%ROOT%\azecotron\app\BUILD.gn" "%APP_DST%\BUILD.gn" >nul
 
 echo Native Azecotron Content API host installed at:
 echo %HOST_DST%
 exit /b 0
 
-copy /Y "%ROOT%\azecotron\app\azecotron_app_main.cc" "%APP_DST%\azecotron_app_main.cc" >nul
-copy /Y "%ROOT%\azecotron\app\BUILD.gn" "%APP_DST%\BUILD.gn" >nul
