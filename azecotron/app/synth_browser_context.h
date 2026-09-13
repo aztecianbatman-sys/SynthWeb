45faf0ba143d649a3fb8fd0f276b517930de9fb2
Fulltrack <aztecianbatman@gmail.com>
2026-09-13 15:22:25 +0530

    fix: clean duplicate includes and fields in SynthBrowserContext

--- azecotron/app/synth_browser_context.h
+++ azecotron/app/synth_browser_context.h
@@ -5,17 +5,10 @@
 
 #include "base/files/file_path.h"
 #include "content/public/browser/browser_context.h"
-#include <memory>
 #include "azecotron/app/synth_download_manager_delegate.h"
 #include "azecotron/app/synth_permission_controller_delegate.h"
 
 namespace synth_azecotron {
-
-class SynthDownloadManagerDelegate;
-class SynthPermissionControllerDelegate;
-
-class SynthDownloadManagerDelegate;
-class SynthPermissionControllerDelegate;
 
 class SynthBrowserContext final : public content::BrowserContext {
  public:
@@ -24,8 +17,7 @@ class SynthBrowserContext final : public content::BrowserContext {
   SynthBrowserContext& operator=(const SynthBrowserContext&) = delete;
   ~SynthBrowserContext() override;
 
-  std::unique_ptr<content::ZoomLevelDelegate> CreateZoomLevelDelegate(
-      const base::FilePath& partition_path) override;
+  std::unique_ptr<content::ZoomLevelDelegate> CreateZoomLevelDelegate(const base::FilePath& partition_path) override;
   base::FilePath GetPath() const override;
   bool IsOffTheRecord() override;
   content::DownloadManagerDelegate* GetDownloadManagerDelegate() override;
@@ -36,23 +28,17 @@ class SynthBrowserContext final : public content::BrowserContext {
   content::StorageNotificationService* GetStorageNotificationService() override;
   content::SSLHostStateDelegate* GetSSLHostStateDelegate() override;
   content::PermissionControllerDelegate* GetPermissionControllerDelegate() override;
-  content::ReduceAcceptLanguageControllerDelegate*
-  GetReduceAcceptLanguageControllerDelegate() override;
-  content::ClientHintsControllerDelegate* GetClientHintsControllerDelegate()
-      override;
+  content::ReduceAcceptLanguageControllerDelegate* GetReduceAcceptLanguageControllerDelegate() override;
+  content::ClientHintsControllerDelegate* GetClientHintsControllerDelegate() override;
   content::BackgroundFetchDelegate* GetBackgroundFetchDelegate() override;
   content::BackgroundSyncController* GetBackgroundSyncController() override;
-  content::BrowsingDataRemoverDelegate*
-  GetBrowsingDataRemoverDelegate() override;
+  content::BrowsingDataRemoverDelegate* GetBrowsingDataRemoverDelegate() override;
 
  private:
   const bool off_the_record_;
   const base::FilePath path_;
   std::unique_ptr<SynthDownloadManagerDelegate> download_delegate_;
   std::unique_ptr<SynthPermissionControllerDelegate> permission_delegate_;
-  std::unique_ptr<SynthPermissionControllerDelegate> permission_delegate_;
-  std::unique_ptr<SynthDownloadManagerDelegate> download_delegate_;
-  const base::FilePath path_;
 };
 
 }  // namespace synth_azecotron
