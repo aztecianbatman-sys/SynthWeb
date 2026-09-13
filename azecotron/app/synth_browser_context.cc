07bd4ba1c42f9708c05cb8f7cd0f017f0e64b58c
Fulltrack <aztecianbatman@gmail.com>
2026-09-13 17:24:47 +0530

    fix: initialize and IO-thread-delete Synth ResourceContext correctly

--- azecotron/app/synth_browser_context.cc
+++ azecotron/app/synth_browser_context.cc
@@ -1,20 +1,28 @@
 #include "azecotron/app/synth_browser_context.h"
 
 #include "content/public/browser/download_manager.h"
+#include "content/public/browser/browser_thread.h"
+#include "base/location.h"
 
 namespace synth_azecotron {
 
 SynthBrowserContext::SynthBrowserContext(
     bool off_the_record,
     const base::FilePath& path)
-    : off_the_record_(off_the_record), path_(path), permission_delegate_(std::make_unique<SynthPermissionControllerDelegate>(path.AppendASCII("synth-policy.json"))), download_delegate_(std::make_unique<SynthDownloadManagerDelegate>(path.AppendASCII("Downloads"))) {}
+    : off_the_record_(off_the_record), path_(path), resource_context_(std::make_unique<content::ResourceContext>()), permission_delegate_(std::make_unique<SynthPermissionControllerDelegate>(path.AppendASCII("synth-policy.json"))), download_delegate_(std::make_unique<SynthDownloadManagerDelegate>(path.AppendASCII("Downloads"))) {}
 
 SynthBrowserContext::~SynthBrowserContext() {
+  NotifyWillBeDestroyed();
   if (auto* manager = GetDownloadManager()) {
     manager->SetDelegate(nullptr);
     manager->Shutdown();
   }
   ShutdownStoragePartitions();
+  auto* resource_context = resource_context_.release();
+  if (resource_context) {
+    content::BrowserThread::DeleteSoon(content::BrowserThread::IO,
+                                       FROM_HERE, resource_context);
+  }
 }
 
 std::unique_ptr<content::ZoomLevelDelegate>
@@ -29,6 +37,10 @@ base::FilePath SynthBrowserContext::GetPath() const {
 
 bool SynthBrowserContext::IsOffTheRecord() {
   return off_the_record_;
+}
+
+content::ResourceContext* SynthBrowserContext::GetResourceContext() {
+  return resource_context_.get();
 }
 
 content::DownloadManagerDelegate*
